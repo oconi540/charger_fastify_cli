bd61fb01997407fdcb7d78218d8e6ad14c9c47ab
oconi540 <165193074+oconi540@users.noreply.github.com>
2024-04-23 13:19:52 +0200

    Añadir funcón getAllCharger para obtener todos los cargadores (#2)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify';
-import getCharger from './getCharger';
 import getMessage from "./getMessage";
+import getCharger from './getCharger';
+import getAllCharger from './getAllCharger';
 
 async function app(fastify: FastifyInstance, opts: any){
   fastify.get('/', getMessage);
+  fastify.get('/charger', getAllCharger);
   fastify.get('/charger/:id', getCharger);
 }
 
